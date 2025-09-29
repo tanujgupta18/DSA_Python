f8bbd1d78ba7d8c1cc1ffaa65b896c5a3c971bcd
Tanuj Gupta <tanujgupta1872003@gmail.com>
2025-09-29 20:15:17 +0530

    Changes

--- Arrays/Q1_Reverse_Array.py
+++ Arrays/Q1_Reverse_Array.py
@@ -2,18 +2,19 @@
 Problem Statement:- You are given an array of integers arr[]. You have to reverse the given array.
 
 '''
-
 class Solution:
     def reverseArray(self, arr):
-        # code here
-        rev = arr[::-1]
-        return rev
-
+        left = 0
+        right = len(arr) - 1
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
 
 sol = Solution()
 
 arr = [5,10,15,20,25]
 
-result = sol.reverseArray(arr)
+sol.reverseArray(arr)
 
-print("Reversed Array:", result)
+print("Reversed Array:", arr)
